c44911d5bd4b229d136a7e3ab247b1db50ad0da9
samuel <samueloyelade6@gmail.com>
2024-05-09 11:20:47 +0100

    react class

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import logo from "./assets/react.svg"
+
+function App() {
+       
+
+  return (
+    <>
+    <div>
+      <div style={{fontSize:"50px", background:""}}>Google</div>
+      < input type="text" className='input'   placeholder='Search Google or type a URL'/>
+
+    </div>
+    </>
+  )
+}
+
+export default App
